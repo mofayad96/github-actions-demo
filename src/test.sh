adf53f2b8e4dce47874577206a27926986bdcb6f
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-09-17 09:35:56 +0300

    1

--- src/test.sh
+++ src/test.sh
@@ -4,7 +4,7 @@
 
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./src/app)('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app.js)('Test'))")
 
 if {"$OUTPUT"=="$EXPECTED" }; then
 echo " test passed!"
