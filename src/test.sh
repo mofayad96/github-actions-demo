032ac12e7bf9f9a905adbb1b3092e54aa6973d75
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-09-17 09:37:09 +0300

    1

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#src/test.sh
+# src/test.sh
 
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./src/app.js)('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app.js')('Test'))")
 
-if {"$OUTPUT"=="$EXPECTED" }; then
-echo " test passed!"
-exit 0
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "test passed!"
+  exit 0
 else
-echo "test failed expected '$EXPEXTED' but got '$OUTPUT'"
-exit 1
-
+  echo "test failed: expected '$EXPECTED' but got '$OUTPUT'"
+  exit 1
 fi
