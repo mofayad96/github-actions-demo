9f2e877596a4a5932dd58e83cb447ff61983a7d6
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-09-17 09:26:40 +0300

    first commit

---
+++ src/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#src/test.sh
+
+EXPECTED="Hello, Test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app)('Test'))")
+
+if {"$OUTPUT"=="$EXPECTED" }; then
+echo " test passed!"
+exit 0
+else
+echo "test failed expected '$EXPEXTED' but got '$OUTPUT'"
+exit 1
+
+fi
